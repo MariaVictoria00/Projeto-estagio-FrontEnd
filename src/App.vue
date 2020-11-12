e214a3cc79fde829688b517f4bde488493db6801
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2020-11-11 20:04:54 -0400

    Ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app" >
-
     <nav>
       <div class="nav-wrapper pink darken-1 col s12 m12 l12">
         <a href="#" class="brand-logo center">Estagiarios</a>
@@ -10,33 +9,31 @@
 
 
     <div class="container col s12 m12 l12">
-      {{estagiario.nome}}
-
+      
+     
       <form class="container col s12 m12 l12" @submit.prevent="salvar">
 
           <label>Nome</label>
           <input class="col s12 m12 l12"  required="" type="text" placeholder="Digite..." v-model="estagiario.nome">
           <label>Telefone</label>
-          <input class="col s12 m12 l12" required="" maxlength="12" type="tel" placeholder="Digite.." v-model="estagiario.telefone">
+          <input class="col s12 m12 l12" required="" maxlength="12" type="number" placeholder="Digite.." v-model="estagiario.telefone">
           <label>E-mail</label>
           <input class="col s12 m12 l12"  required="" type="text" placeholder="Digite.." v-model="estagiario.email">
           <label> Foto</label>   
-          <input class="col s12 m12 l12"  required="" type="file" accept="image/*" placeholder="url" width:1rem estagiario.foto>
+          <input class="col s12 m12 l12"  required="" type="url"  placeholder="url" v-model="estagiario.foto">
           <label>Comprovante de matricula</label>
-          <input class="col s12 m12 l12"  required="" type="file" placeholder="url" estagiario.comprovanteDeMatricula> 
+          <input class="col s12 m12 l12"  required="" type="url" placeholder="url" v-model="estagiario.comprovanteDeMatricula"> 
           <label>Areas de interesse</label>
           <input class="col s12 m12 l12" required="" type="text" placeholder="Digite.." v-model="estagiario.areasDeInteresse">
           <label>Areas de dominio</label>
           <input class="col s12 m12 l12"  required="" type="text" placeholder="Digite.." v-model="estagiario.areasDeDominio">
 
-
           <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+     
       </form>
 
       <table class="responsive-table">
-
         <thead>
-
           <tr>
             <th>NOME</th>
             <th>TELEFONE</th>
@@ -46,20 +43,19 @@
             <th>A.INTERESSE</th>
             <th>A.DOMINIO</th>
           </tr>
-
         </thead>
 
         <tbody>
 
           <tr v-for="estagiario of estagiarios" :key="estagiario.id">
 
-            <td>{{estagiario.nome | truncate(6)}}</td>
-            <td>{{estagiario.telefone | truncate(6) }}</td>
-            <td>{{estagiario.email | truncate(6) }}</td>
-            <td>{{estagiario.foto | truncate(6) }}</td>
-            <td>{{estagiario.comprovanteDeMatricula | truncate(6) }}</td>
-            <td>{{estagiario.areasDeInteresse | truncate(6) }}</td>
-            <td>{{estagiario.areasDeDominio | truncate(6) }}</td>
+            <td>{{estagiario.nome | truncate(7) }}</td>
+            <td>{{estagiario.telefone | truncate(7) }}</td>
+            <td>{{estagiario.email | truncate(7) }}</td>
+            <td><img src="estagiario.foto"></td>
+            <td><img src="estagiario.comprovanteDeMatricula "></td>
+            <td>{{estagiario.areasDeInteresse | truncate(8) }}</td>
+            <td>{{estagiario.areasDeDominio| truncate(7) }}</td>
             <td>
               <button @click="editar(estagiario)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
               <button @click="remover(estagiario)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -68,13 +64,11 @@
           </tr>
 
         </tbody>
-      
       </table>
-
     </div>
-
   </div>
 </template>
+
 <script>
  
  import Controle from './services/estagiarios'
@@ -93,19 +87,16 @@
           areasDeDominio: ''
         },
 
-    //armazenar dados da api no array
-        estagiarios:[]
+        estagiarios:[]   //armazenar dados da api no array
       }
 
     },
-    
     //metodo que contem a api
     mounted(){
       this.listar()
-     
     },
     methods:{
-     
+    
       //atualizar a pagina remotamente
       listar(){
          Controle.listar().then(resposta =>{
@@ -170,33 +161,15 @@
 <style>
 html{
 overflow-y:auto;
-}
+overflow-x: hidden; 
 
-/* ajustando a label e input do formulário*/
-label{
-  display: block;
 }
-input {
-    padding: 100px 100px;
-}
-input[type=file] {
-    /* width: 100%; */
-    max-width: 50%;
-    padding: 12px 0px;
-    margin: 0px 0px 0px 0px; 
-    box-sizing: border-box;
-    font-size: 10;  
-    
-}
-/* botao de upload */
- .container {
-  padding-top: 30px;
+  .container {
+  padding-top: 10px;
   font-size:12px;
   white-space: 12px;
-  
+ 
  }
-
-
 
 
 
